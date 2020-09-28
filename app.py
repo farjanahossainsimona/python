09766869a7da52c28aa1ce10cd0d5f000f1f1b05
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-28 09:11:39 +0600

    string

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-x = [1, 2, 3]
-print(id(x))
+course = "Python Programming"
+print(len(course))  # number of character in this string
+print(course[0])  # to get the first character
+print(course[-1])  # to get last character
+print(course[0:3])  # to get first 3 character
+print(course[:3]) #same as before
+print(course[0:]) #entire string
+print(course[:]) #same as before
 
-x.append(4)
-print(id(x))
+#string are immutable,let's verify this
+print(id(course))
+print(id(course[0]))
+
+
+ 
+
