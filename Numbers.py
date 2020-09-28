267d5f6f20b6a394a915e0c77c7a288c5904f377
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-28 16:49:23 +0600

    numbers

---
+++ Numbers.py
@@ -0,0 +1,12 @@
+x = 10
+x = 0b10
+print(x)  # return decimal
+print(bin(x))  # return binary
+
+x = 0x12c
+print(bin(x))
+
+# a + bi
+# j/J means i or imaginary number on math
+x = 1 + 2j
+print(x)
