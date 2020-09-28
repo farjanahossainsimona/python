154e6218120e0674d603998911830932c47aa792
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-28 14:23:13 +0600

    usefulstringmethods

--- usefulstringmethods.py
+++ usefulstringmethods.py
@@ -1,4 +1,3 @@
-
 course = "  Python Programming"
 # dot notation: to find all the method
 print(course.upper())
@@ -7,3 +6,8 @@ print(course.title())
 
 print(course.strip())
 print(course.find("Pro"))
+print(course.replace("P", "-"))
+
+print("Progamming" in course)
+print("Programming" in course)
+print("Programming" not in course)
