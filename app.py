8c09d366088119c30e2647718a34f7bbdae572f3
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-28 09:32:19 +0600

    Escape Squences

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-course = "Python Programming"
-print(len(course))  # number of character in this string
-print(course[0])  # to get the first character
-print(course[-1])  # to get last character
-print(course[0:3])  # to get first 3 character
-print(course[:3]) #same as before
-print(course[0:]) #entire string
-print(course[:]) #same as before
-
-#string are immutable,let's verify this
-print(id(course))
-print(id(course[0]))
-
-
- 
-
+# to display another " middle of the string
+message = "Python \"Programming"
+print(message)
+# to display ' middle of the string
+message = "Python \'Programming"
+print(message)
+# to display \ middle of the string
+message = "Python \\Programming"
+print(message)
+# for new line
+message = "Python \nProgramming"
+print(message)
+# better to use """ """ instead of n\ otherwise that's look ugly
+message = """Python
+Programming
+"""
+print(message)
