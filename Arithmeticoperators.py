751d4de27e513becf680a7300534aa31f0f4d474
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-28 17:17:35 +0600

    Arithmeticoperators

---
+++ Arithmeticoperators.py
@@ -0,0 +1,11 @@
+x = 10 + 3
+x = 10 - 3
+x = 10 * 3
+x = 10 / 3
+x = 10 // 3  # to get return intiger value
+x = 10 % 3  # Remainder of devision
+x = 10 ** 3  # exponent
+"""x = x + 1
+x += 1 both are same """
+
+print(x)
