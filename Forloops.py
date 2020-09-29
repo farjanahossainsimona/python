f4c05cf8e02c0dd3b0f7aa485608f1116cddd110
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-29 14:21:09 +0600

    forloops

---
+++ Forloops.py
@@ -0,0 +1,15 @@
+"""for x in "Python":
+    print(x)
+for x in ['a', 'b', 'c']:
+    print(x)
+for x in range(5):
+    print(x)
+for x in range(2, 5):
+    print(x)
+for x in range(0, 10, 2):
+    print(x)"""
+# diference betweet range and list
+# range function return range object
+print(range(5))
+print(type(range))
+print([1, 2, 3, 4])
