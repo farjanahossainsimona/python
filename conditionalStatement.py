aa6b077ac31d3f29087e5ed0b910c64e1a572ab6
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-29 11:25:14 +0600

    conditionStatement

---
+++ conditionalStatement.py
@@ -0,0 +1,15 @@
+"""age = 22
+if age >= 18:
+    print("Adult")
+    print("Adult")
+elif age >= 13:
+    print("Teenager")
+else:
+    print("child")
+
+
+print("All done!")"""
+if x > 1:
+    pass  # if i want empty block ,should use pass keyword
+else:
+    pass
