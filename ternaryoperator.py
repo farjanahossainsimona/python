259c0dffa79b73e3311c2b20e363ed6ebf7378fd
farjanahossain <farjana.96.hossain@gmail.com>
2020-09-29 13:28:06 +0600

    ternaryOperator

---
+++ ternaryoperator.py
@@ -0,0 +1,10 @@
+age = 22
+"""
+if age >= 18:
+    message = "Eligible"
+else:
+    message = "Not Eligible"""
+# In python way:ternaryOperator
+message = "Eligible" if age >= 18 else "Not Eligible"
+
+print(message)
